e079feb2730109e818b1b481f51bb289c7919834
keshavdhingra451 <keshavdhingra451@gmail.com>
2026-06-23 12:34:09 +0530

    Initial Flask application

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "<h1>Welcome to DSA Notes</h1>"
+
+@app.route("/arrays")
+def arrays():
+    return render_template("arrays.html")
+
+@app.route("/linkedlists")
+def linkedlists():
+    return render_template("linkedlists.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
